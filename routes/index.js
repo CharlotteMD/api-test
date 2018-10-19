2b59a58ec2b0cf8f92ad65a1767861893855bc00
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-10-19 18:09:40 +0100

    added routes for practise

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', char: 'Hello Charlotte' });
+});
+
+router.get('/results', function(req, res, next) {
+  res.render('results', { char: 'Hello Charlotte', results: 'Here are your results' });
 });
 
 module.exports = router;
